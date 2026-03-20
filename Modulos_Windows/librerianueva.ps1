ed826ae4644bc2a456dc90b8bd6126936e12f3d5
Alberto-DOBLEH <luis.alberto.hdez.hdez245@gmail.com>
2026-03-20 15:51:13 -0700

    IStart

--- Modulos_Windows/librerianueva.ps1
+++ Modulos_Windows/librerianueva.ps1
@@ -816,7 +816,7 @@ function instalar_apache {
         $headers = @{
             "User-Agent" = "Mozilla/5.0"
         }
-        IStart-BitsTransfer -Source $jdkUrl -Destination $jdkZipPath
+        Start-BitsTransfer -Source $jdkUrl -Destination $jdkZipPath
         Write-Host "Apache descargado en: $destinoZip"
 
         # Extraer Apache en C:\Apache24
